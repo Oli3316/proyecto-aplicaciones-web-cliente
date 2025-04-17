55e9c605347119498985558e65df0efacbf57c34
Oliver <oliverbaker3316@gmail.com>
2025-04-17 12:41:30 -0300

    Add changes

---
+++ scripts/app.js
@@ -0,0 +1,57 @@
+const productos = [
+    {
+        name: "Cybertruck",
+        description: "A bold, angular electric pickup truck built with ultra-hard stainless steel. Designed for durability, off-road capability, and futuristic appeal.",
+        image: "https://www.blessthisstuff.com/imagens/stuff/tesla-cybertruck-2.jpg" ,
+        price: 125.000,
+        available: 8
+    },
+    {
+        name: "Model S",
+        description: "A luxury all-electric sedan offering impressive range, cutting-edge tech, and supercar-like acceleration. Ideal for those seeking performance and elegance",
+        image:  ,
+        price: 35.000,
+        available: 12
+    },
+    {
+        name: "Model 3",
+        description: "A sleek, compact sedan known for its affordability, efficiency, and minimalist design. One of Tesla’s best-selling models globally",
+        image:  ,
+        price: 55.000,
+        available: 15
+    },
+    {
+        name: "Model X",
+        description: "A full-size electric SUV featuring signature falcon-wing rear doors, advanced safety features, and spacious seating for up to seven.",
+        image:  ,
+        price: 23.000,
+        available: 19
+    },
+    {
+        name: "Model Y",
+        description: " A compact crossover SUV that blends practicality and performance. It offers ample cargo space, great range, and family-friendly design.",
+        image:  ,
+        price: 47.000,
+        available: 6
+    },
+    {
+        name: "Roadster (upcoming)",
+        description: "Tesla’s next-gen sports car, expected to deliver record-breaking acceleration and range. Aimed at redefining electric performance.",
+        image:  ,
+        price: ,
+        available: 0
+    },
+    {
+        name: "Semi",
+        description: " A powerful electric truck for commercial freight. It combines long-range capabilities, reduced operating costs, and enhanced driver comfort..",
+        image:  ,
+        price: 26.000,
+        available: 9
+    },
+]
+
+const grid = document.querySelector('.product-section');
+
+function createProductCard (product) {
+    
+}
